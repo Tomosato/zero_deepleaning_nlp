c6428e73cbc67e09283d717bd275b6da14972665
Tomosato <saitama-saitama@hotmail.co.jp>
2020-05-26 14:42:51 +0900

    TwoLayerNetの実装

--- ch1/forward_net.py
+++ ch1/forward_net.py
@@ -15,3 +15,29 @@ class Affine:
     def forward(self, x):
         W, b = self.params
         return np.dot(x, W) + b
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(0)
+
+        # レイヤの作成
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        # 全ての重みをリストにまとめる
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
